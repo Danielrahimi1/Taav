2e9624a805ef3c95dc762f8fcbb5668aac93e6f8
danielrahimi1 <danielrahimi1@gmail.com>
2024-08-05 15:32:48 +0330

    Fixed non-nullable property warning

--- GameApp/Game.cs
+++ GameApp/Game.cs
@@ -3,6 +3,6 @@ namespace Games;
 public abstract class Game
 {
     public abstract string Name { get; init; }
-    public virtual string Description { get; init; }
+    public virtual string Description { get; init; } = "";
     public abstract void Play();
 }
