d351495fd342a4bd6b85ec0ae2690f7c4dc9ae74
danielrahimi1 <danielrahimi1@gmail.com>
2024-08-06 11:14:14 +0330

    List<Game> as GameNet arg, Exit menu

--- GameApp/GameNet.cs
+++ GameApp/GameNet.cs
@@ -4,10 +4,16 @@ namespace Gamenet;
 public class GameNet
 {
     private string _player;
-    private List<Game> _games = [];
-    public GameNet(string player)
+    private List<Game> _games;
+
+    // To be implemented
+    // Params Collection
+    // https://learn.microsoft.com/en-us/dotnet/csharp/language-reference/proposals/params-collections
+    public GameNet(string player, List<Game> games = null)
     {
         _player = player;
+        _games = games;
+        _games ??= [];
     }
 
     public void AddGame(Game game)
@@ -34,8 +40,9 @@ public class GameNet
     {
         for (int i = 0; i < _games.Count; i++)
         {
-            Console.WriteLine($"{i + 1}. {_games[i].Name} - {_games[i].Description}");
+            WriteLine($"{i + 1}. {_games[i].Name} - {_games[i].Description}");
         }
+        WriteLine($"-1. Exit");
     }
 
     private int GetChoice()
@@ -44,10 +51,8 @@ public class GameNet
         {
             if (int.TryParse(ReadLine()!, out int userInput))
             {
-                if (1 <= userInput && userInput <= _games.Count)
-                {
-                    return userInput;
-                }
+                if (userInput == -1) Environment.Exit(0);
+                if (1 <= userInput && userInput <= _games.Count) return userInput;
             }
         }
     }
