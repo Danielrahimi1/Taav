c0ccce344a98d4428a14a62cf4e6bfcd4a0499e0
danielrahimi1 <danielrahimi1@gmail.com>
2024-08-06 11:16:30 +0330

    Gamenet ctor games use

--- GameApp/Program.cs
+++ GameApp/Program.cs
@@ -1,15 +1,25 @@
 ﻿using Games;
 using Gamenet;
 
-var gameNet = new GameNet("John");
+var games = new List<Game>{
+    new GuessNumber(),
+    new GuessNumber(),
+    new GuessNumber(),
+    new GuessNumber(),
+    new GuessNumber(),
+};
 
-gameNet.AddGame(new GuessNumber());
-gameNet.AddGame(new GuessNumber("Rockstar Guess Number"));
-gameNet.AddGame(new GuessNumber("Blizzard Guess Number"));
-gameNet.AddGame(new GuessNumber("CDPR Guess Number"));
-gameNet.AddGame(new GuessWord());
-gameNet.AddGame(new GuessWord("Ubisoft Guess Word"));
-gameNet.AddGame(new GuessWord("FromSoftware Guess Word"));
-gameNet.AddGame(new GuessWord("Bethesda Guess Word"));
+var gameNet = new GameNet("John", games);
+
+// gameNet.AddGame(new GuessNumber());
+// gameNet.AddGame(new GuessNumber("Rockstar Guess Number"));
+// gameNet.AddGame(new GuessNumber("Blizzard Guess Number"));
+// gameNet.AddGame(new GuessNumber("CDPR Guess Number"));
+// gameNet.AddGame(new GuessWord());
+// gameNet.AddGame(new GuessWord("Ubisoft Guess Word"));
+// gameNet.AddGame(new GuessWord("FromSoftware Guess Word"));
+// gameNet.AddGame(new GuessWord("Bethesda Guess Word"));
+
+
 
 gameNet.ComeIn();
